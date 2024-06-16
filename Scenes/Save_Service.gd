0bf549d7f87d6af20603b0b216a36ab0ce8aebf9
Jacob Bernard <jbernard3396@gmail.com>
2024-06-16 11:39:54 -0400

    test master push

--- Scenes/Save_Service.gd
+++ Scenes/Save_Service.gd
@@ -32,7 +32,7 @@ func loadGame():
 	load_crop_game_state(save_game)
 	clock.passTime(timeToTick);
 	EventBus.emit_signal("gameLoaded")
-	print('test vcs #3');
+	print('ensuring we cannot push to master');
 	
 	
 	
